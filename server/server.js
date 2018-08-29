0eaf044567b7620b8e273457d516c82ddf8649c3
Sam Johnston <samuelalbertjohnston@gmail.com>
2018-08-29 15:49:07 -0600

    server changes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express'),
 
 const app = express();
 
-const SERVER_PORT = 3005
+const SERVER_PORT = 3006
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server listening on port ${SERVER_PORT}.`)
